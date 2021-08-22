49161901e8e8326ad1a785ff914cd375f7783a3c
strendz <89350718+strendz@users.noreply.github.com>
2021-08-22 19:18:18 +0200

    testowaniefunkci

---
+++ testowaniefunkcji.py
@@ -0,0 +1,14 @@
+#zadanie
+#wez od uzytkownika liczbe i wypisal jaki jest jej kwadrat pozniej wez druga liczbe i jaki jest jej kwadrat bez uzycia petli
+
+pierwszaliczba = input("Podaj pierwsza liczba: ")
+
+pierwszaliczba = int(pierwszaliczba)
+
+print(pierwszaliczba*pierwszaliczba)
+
+drugaliczba = input("Podaj druga liczba: ")
+
+drugaliczba = int(drugaliczba)
+
+print(drugaliczba*drugaliczba)
