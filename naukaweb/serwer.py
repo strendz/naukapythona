690c7285cb59eb94fe00be579a8a148752b3aeb6
strendz <89350718+strendz@users.noreply.github.com>
2021-08-22 18:24:50 +0200

    Create serwer.py

---
+++ naukaweb/serwer.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def helloworld():
+    return 'Hello, World'
+
+@app.route("/<name>")
+
+def hello(name):
+    return f"Hello, {escape(name)}!"
